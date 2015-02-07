7a5dda570dc459bef9b163be29393a302573f4d1
German Larrain <glarrain@users.noreply.github.com>
2015-02-07 20:22:42 -0300

    add module `hex`
    Functions and constants about hex values, UUID, hashes and random values.

---
+++ lu_dj_utils/hex.py
@@ -0,0 +1,48 @@
+# coding: utf-8
+"""Functions and constants about hex values, UUID, hashes and random values.
+
+Source: lookup_www.common.utils.hash
+
+"""
+from __future__ import absolute_import, print_function, unicode_literals
+
+import hashlib
+import re
+import uuid
+
+
+HEX_6_RE = re.compile('^[a-f0-9]{6}$')  # 6 hex digits string
+HEX_32_RE = re.compile('^[a-f0-9]{32}$')  # 32 hex digits string
+HEX_40_RE = re.compile('^[a-f0-9]{40}$')  # 40 hex digits string
+SHA1_RE = HEX_40_RE
+UUID4_RE = HEX_32_RE
+
+
+def hex_sha1(salt, value):
+    """Return a 40-character string containing only hexadecimal digits based on
+    the string resulting from the concatenation of strings `salt` and `value`.
+    These can be regular or unicode strings but non-ascii characters can not be
+    used.
+
+    Equivalent to hashlib.sha1(salt + value).hexdigest()
+
+    """
+    m = hashlib.sha1()
+    m.update(salt)
+    m.update(value)
+    return m.hexdigest()
+
+
+def random_hex_32():
+    """Return a 32-character string containing only hexadecimal digits based on
+    a random generated version-4 UUID.
+
+    """
+    return uuid.uuid4().get_hex()
+
+
+def random_hex_6():
+    """Return a string of 6 hexadecimal digits based on :func:`random_hex_32`.
+
+    """
+    return random_hex_32()[:6]
---
+++ tests/test_hex.py
@@ -0,0 +1,59 @@
+# coding: utf-8
+from __future__ import absolute_import, print_function, unicode_literals
+
+import unittest
+
+
+class FunctionsTest(unittest.TestCase):
+
+    def test_hex_sha1(self):
+        from lu_dj_utils.hex import hex_sha1, SHA1_RE
+
+        self.assertEqual(
+            hex_sha1('', ''),
+            'da39a3ee5e6b4b0d3255bfef95601890afd80709')
+        self.assertEqual(
+            hex_sha1('chao1', 'hola'),
+            'bf01a6b75b943b6be4b0d5cce120d5cee9559217')
+        self.assertEqual(
+            hex_sha1('hola', 'chao1'),
+            '61807d12b62246bbe5f46955746b9a4ffbc6fb0d')
+        self.assertEqual(
+            hex_sha1(u'hola', u'chao1'),
+            '61807d12b62246bbe5f46955746b9a4ffbc6fb0d')
+
+        # test results match the corresponding regex
+        for i in range(0, 10000):
+            value = hex_sha1('', str(i))
+            self.assertIsNotNone(SHA1_RE.match(value))
+
+    def test_random_hex_32(self):
+        # values random enough such that there are no repetitions in 10,000 calls
+
+        from lu_dj_utils.hex import random_hex_32, HEX_32_RE
+
+        keys = {}
+        size_ = 10000
+        for i in range(0, size_):
+            random_key = random_hex_32()
+            keys[random_key] = None
+            # test results match the corresponding regex
+            self.assertIsNotNone(HEX_32_RE.match(random_key))
+        # if there were any repeated keys then len(keys) < size_
+        self.assertEquals(size_, len(keys))
+
+    def test_random_hex_6(self):
+        # values random enough such that there are no repetitions in 100 calls
+        # (with 1,000 it fails frequently enough to be annoying)
+
+        from lu_dj_utils.hex import random_hex_6, HEX_6_RE
+
+        keys = {}
+        size_ = 100
+        for i in range(0, size_):
+            random_key = random_hex_6()
+            keys[random_key] = None
+            # test results match the corresponding regex
+            self.assertIsNotNone(HEX_6_RE.match(random_key))
+        # if there were any repeated keys then len(keys) < size_
+        self.assertEquals(size_, len(keys))

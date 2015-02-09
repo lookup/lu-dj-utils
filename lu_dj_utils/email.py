28996eeded6e270bc47384cef2a5dbd4a94e5c76
German Larrain <glarrain@users.noreply.github.com>
2015-02-09 00:25:05 -0300

    add module `email`
    Includes classes `PlainEmail` and `HTMLEmail`, and functions `send_mail2`,
    `send_templated_mail`, `set_header_reply_to` and `validate_addresses`.

---
+++ lu_dj_utils/email.py
@@ -0,0 +1,265 @@
+# coding: utf-8
+"""Email-related utilities to create, render and send messages.
+
+Source: lookup_www.common.utils.test (with some edition).
+
+"""
+from __future__ import absolute_import, print_function, unicode_literals
+
+import logging
+
+import django.core.mail
+from django.template.loader import render_to_string
+
+
+logger = logging.getLogger(__name__)
+
+
+class PlainEmail(object):
+
+    """Friendly wrapper over :class:`django.core.mail.EmailMessage`
+
+    .. note::
+        The only public attribute is :attr:`connection` because it can be
+        changed safely after instantiation.
+
+    """
+
+    django_mail_class = django.core.mail.EmailMessage
+
+    def __init__(
+            self, subject='', body='', from_email=None, to=None, cc=None,
+            bcc=None, reply_to=None, connection=None, attachments=None,
+            headers=None):
+
+        self._subject = subject
+        self._body = body
+
+        self._from_email = from_email
+        self._to = to
+        self._cc = cc
+        self._bcc = bcc
+
+        self.connection = connection
+        self._attachments = attachments
+
+        headers = set_header_reply_to(headers, reply_to)
+        self._headers = headers
+
+    def create_message(self):
+        """Create an email message.
+
+        :return: email message object
+        :rtype: :attr:`django_mail_class`
+
+        """
+        return self.django_mail_class(
+            subject=self._subject,
+            body=self._body,
+            from_email=self._from_email,
+            to=self._to,
+            bcc=self._bcc,
+            connection=self.connection,
+            attachments=self._attachments,
+            headers=self._headers,
+            cc=self._cc)
+
+    def send(self):
+        """Create the email message and send it.
+
+        :return: the number of email messages sent (return value of :meth:`EmailMessage.send`)
+        :rtype: int
+
+        """
+        message = self.create_message()
+        return message.send()
+
+
+class HTMLEmail(PlainEmail):
+
+    """Customization of :class:`PlainEmail` to send a multipart HTML email."""
+
+    django_mail_class = django.core.mail.EmailMultiAlternatives
+    _EMAIL_ALTERNATIVE_MIME_TYPE = 'text/html'
+
+    def __init__(self, html_body, *args, **kwargs):
+        super(HTMLEmail, self).__init__(*args, **kwargs)
+        self._html_body = html_body
+
+    def create_message(self):
+        message = super(HTMLEmail, self).create_message()
+        message.attach_alternative(self._html_body, self._EMAIL_ALTERNATIVE_MIME_TYPE)
+        return message
+
+
+def send_mail2(
+        subject='', body='', from_email=None, to=None, cc=None, bcc=None,
+        reply_to=None, fail_silently=False, auth_user=None,
+        auth_password=None, connection=None, attachments=None, headers=None):
+    """Combination of :func:`django.core.mail.send_mail` and
+    :class:`django.core.mail.EmailMessage`.
+
+    The original ``send_mail`` was too limited and kind of deprecated ('The API
+    for this method is frozen'). This function augments it.
+
+    These are the parameters differences:
+    * ``message`` renamed to ``body`` (compliant with ``EmailMessage``)
+    * ``recipient_list`` renamed to ``to`` (compliant with ``EmailMessage``)
+    * added ``cc``, ``bcc``, ``reply_to``, ``attachments`` and ``headers``
+    * set default values for ``subject``, ``message``/``body``, ``from_email``,
+        ``recipient_list``/``to``
+
+    Check parameters definitions at :class:`django.core.mail.EmailMessage`
+    and :func:`django.core.mail.get_connection`.
+
+    :return: the number of email messages sent (return value of
+        :meth:`EmailMessage.send`)
+
+    .. warning::
+        All passed email addresses must be validated beforehand. For example,
+        all these values **WILL** result in a message being sent
+        (i.e. return value will be ``> 0``:
+        ``['', ' ', 'x', 'x@x']``
+        Other values may raise exceptions e.g. ValueError and
+        its subclass BadHeaderError.
+
+    .. warning::
+        Do not confuse ``from_email`` with ``auth_user``.
+
+    """
+    # 1. basic validation of given addresses lists/tuples
+    # 2. get the connection
+    # 3. set header 'Reply-To' (if given)
+    # 4. create the EmailMessage object
+    # 5. send it
+
+    if to is not None:
+        validate_addresses(to)
+    if cc is not None:
+        validate_addresses(cc)
+    if bcc is not None:
+        validate_addresses(bcc)
+
+    if connection is None:
+        connection = django.core.mail.get_connection(
+            username=auth_user,
+            password=auth_password,
+            fail_silently=fail_silently)
+
+    headers = set_header_reply_to(headers, reply_to)
+    message = django.core.mail.EmailMessage(
+        subject, body, from_email, to, bcc, connection, attachments, headers, cc)
+
+    return message.send()
+
+
+def send_templated_mail(
+        context, subject_template_name, body_template_name, html_body_template_name=None,
+        **kwargs):
+    """Send mail by rendering subject and body templates using ``context``.
+
+    The message will be a plain text or HTML multipart email depending on
+    whether ``html_body_template_name`` is None or not.
+
+    ``kwargs`` is passed directly to :class:`PlainEmail`, allowing full
+    customization of the email creation (e.g. to, cc, bcc, headers, attachments)
+    and send (i.e. connection object) process.
+
+    :param context: context data used to render the templates
+    :type context: :class:`dict`
+    :param subject_template_name: the name of the subject template
+    :type subject_template_name: string
+    :param body_template_name: the name of the plain-text body template
+    :type body_template_name: string
+    :param html_body_template_name: if a multipart HTML email is desired, the
+        name of the HTML body template; None otherwise
+    :type html_body_template_name: string or None
+    :return: True if at least one message is sent successfully; False otherwise
+    :rtype: bool
+
+    .. warning::
+        All passed email addresses **must** be validated beforehand.
+
+    .. warning::
+        ``to``, ``cc`` and ``bcc`` arguments must be **iterables** of strings,
+        not plain strings, no matter if their length is 1.
+
+    """
+    #noinspection PyBroadException
+    try:
+        subject = render_to_string(subject_template_name, context)
+        subject = ''.join(subject.splitlines())
+        body = render_to_string(body_template_name, context)
+        if html_body_template_name is None:
+            html_body = None
+        else:
+            html_body = render_to_string(html_body_template_name, context)
+    except Exception:
+        logger.exception(
+            "Failed to render body or subject.\nTemplate names: (%s, %s, %s)\nContext: %s" %
+            (subject_template_name, body_template_name, html_body_template_name, context))
+        return False
+
+    #noinspection PyBroadException
+    try:
+        if html_body is None:
+            message = PlainEmail(subject=subject, body=body, **kwargs)
+        else:
+            message = HTMLEmail(html_body, subject=subject, body=body, **kwargs)
+
+        result = message.send()
+        if not result >= 1:
+            logger.error("No mail was sent.\nsubject: %s\nbody: %s\nkwargs: %s" %
+                         (subject, body, kwargs))
+            return False
+    except Exception:
+        logger.exception("send_templated_mail failed.\nsubject: %s\nbody: %s\nkwargs: %s" %
+                         (subject, body, kwargs))
+        return False
+    return True
+
+
+def set_header_reply_to(headers, reply_to):
+    """Set ``reply_to`` as email header ``'Reply-To'``.
+
+    :param headers: email headers
+    :type headers: dict or None
+    :param reply_to: email address to set as header ``'Reply-To'``
+    :type reply_to: basestring or None
+    :return: updated headers, if ``reply_to`` is not None
+    :rtype: dict, if ``reply_to`` is not None; unknown otherwise
+
+    >>> set_header_reply_to(None, 'a@a.com')
+    {'Reply-To': 'a@a.com'}
+    >>> set_header_reply_to({}, 'a@a.com')
+    {'Reply-To': 'a@a.com'}
+    >>> set_header_reply_to({'Reply-To': 'a@a.com'}, 'b@b.com')
+    {'Reply-To': 'b@b.com'}
+    >>> set_header_reply_to(None, None)
+    None
+    >>> set_header_reply_to({}, None)
+    {}
+
+    """
+    if reply_to is not None:
+        if headers is None:
+            headers = {'Reply-To': reply_to}
+        else:
+            headers['Reply-To'] = reply_to
+    return headers
+
+
+def validate_addresses(addresses):
+    """Check that ``addresses`` can be joined in a single string.
+
+    :param addresses: email addresses
+    :rtype: list or tuple
+    :raises: ValueError if ``addresses`` couldn't be joined
+
+    """
+    addresses = list(addresses)
+    try:
+        ', '.join(addresses)
+    except TypeError:
+        raise ValueError("Email addresses couldn't be joined in a single string: %s\n"
+                         "Hint: probably one of them is not a string" % addresses)
---
+++ tests/test_email.py
@@ -0,0 +1,419 @@
+# coding: utf-8
+"""Source: lookup_www.common.tests.test_utils.mail (with some edition).
+
+"""
+from __future__ import absolute_import, print_function, unicode_literals
+
+from mock import MagicMock, Mock, NonCallableMock, NonCallableMagicMock, patch
+
+from django.conf import settings
+from django.core import mail as django_mail
+from django.test import SimpleTestCase, TestCase
+
+
+class PlainEmail(SimpleTestCase):
+
+    def setUp(self):
+        NCMock = NonCallableMock
+
+        self.subject = NCMock(spec=str)
+        self.body = NCMock(spec=str)
+        self.from_email = NCMock(spec=str)
+
+        # self.to = NonCallableMagicMock(spec=list)
+        # self.to.__iter__.return_value = ['to0', 'to1', 'to2']
+        # self.cc = NonCallableMagicMock(spec=list)
+        # self.cc.__iter__.return_value = ['cc0', 'cc1', 'cc2']
+        # self.bcc = NonCallableMagicMock(spec=list)
+        # self.bcc.__iter__.return_value = ['bcc0', 'bcc1', 'bcc2']
+
+        self.to = ['to0', 'to1', 'to2']
+        self.cc = ['cc0', 'cc1', 'cc2']
+        self.bcc = ['bcc0', 'bcc1', 'bcc2']
+
+        self.reply_to = NCMock(spec=str)
+        self.fail_silently = NCMock(spec=bool)
+        self.auth_user = NCMock(spec=str)
+        self.auth_password = NCMock(spec=str)
+        self.connection = NCMock()
+        self.attachments = NCMock(spec=list)
+        self.headers = NonCallableMagicMock(spec=dict)
+
+    def test_create_message(self):
+        from lu_dj_utils.email import PlainEmail
+
+        msg = PlainEmail().create_message()
+        self.assertIsInstance(msg, PlainEmail.django_mail_class)
+
+        msg = PlainEmail(
+            subject=self.subject, body=self.body, from_email=self.from_email,
+            to=self.to, bcc=self.bcc, connection=self.connection,
+            attachments=self.attachments, headers=self.headers, cc=self.cc
+        ).create_message()
+        self.assertIsInstance(msg, PlainEmail.django_mail_class)
+        self.basic_msg_assertions(msg)
+
+    def test_real_send(self):
+        from lu_dj_utils.email import PlainEmail
+
+        _reset_test_outbox()
+        self.assertEqual(len(django_mail.outbox), 0)
+
+        subject = 'hello!'
+        body = 'my email body'
+        to = ['a@a.cl', 'x@xx.com']
+
+        result = PlainEmail(subject=subject, body=body).send()
+        self.assertEqual(result, 0)
+
+        result = PlainEmail(subject=subject, body=body, to=to).send()
+        self.real_send_assertions(to, body, result)
+
+    def basic_msg_assertions(self, msg):
+        self.assertEqual(self.subject, msg.subject)
+        self.assertEqual(self.body, msg.body)
+        self.assertEqual(self.from_email, msg.from_email)
+        self.assertEqual(self.to, msg.to)
+        self.assertEqual(self.cc, msg.cc)
+        self.assertEqual(self.bcc, msg.bcc)
+        self.assertEqual(self.connection, msg.connection)
+        self.assertEqual(self.attachments, msg.attachments)
+        self.assertDictContainsSubset(self.headers, msg.extra_headers)
+
+    def real_send_assertions(self, to, body, result):
+        outbox = django_mail.outbox
+        self.assertEqual(result, 1)
+        self.assertEqual(len(outbox), 1)
+        self.assertEqual(outbox[0].to, to)
+        self.assertIn(body, outbox[0].body)
+
+
+class HTMLEmail(PlainEmail):
+
+    def setUp(self):
+        super(HTMLEmail, self).setUp()
+        self.html_body = NonCallableMock(spec=str)
+
+    def test_create_message(self):
+        from lu_dj_utils.email import HTMLEmail
+
+        msg = HTMLEmail(self.html_body).create_message()
+        self.assertIsInstance(msg, HTMLEmail.django_mail_class)
+
+        msg = HTMLEmail(
+            self.html_body,
+            subject=self.subject, body=self.body, from_email=self.from_email,
+            to=self.to, bcc=self.bcc, connection=self.connection,
+            attachments=self.attachments, headers=self.headers, cc=self.cc
+        ).create_message()
+        self.assertIsInstance(msg, HTMLEmail.django_mail_class)
+        self.basic_msg_assertions(msg)
+        self.assert_HTML_alternative(msg, self.html_body)
+
+    def test_real_send(self):
+        from lu_dj_utils.email import HTMLEmail
+
+        _reset_test_outbox()
+        self.assertEqual(len(django_mail.outbox), 0)
+
+        subject = 'hello!'
+        body = 'my email body'
+        to = ['a@a.cl', 'x@xx.com']
+
+        with open(_template_file_abspath('test_html_body.html'), 'r') as f:
+            html_body = f.read()
+            result = HTMLEmail(
+                html_body, subject=subject, body=body).send()
+            self.assertEqual(result, 0)
+
+            result = HTMLEmail(
+                html_body, subject=subject, body=body, to=to).send()
+            self.real_send_assertions(to, body, result)
+            self.assert_HTML_alternative(django_mail.outbox[0], html_body)
+
+    def assert_HTML_alternative(self, message, html_body):
+        html_alternative = message.alternatives[0]
+        self.assertEqual(html_alternative[0], html_body)
+        self.assertEqual(html_alternative[1], 'text/html')
+
+
+# noinspection PyUnresolvedReferences
+class SendMail2(SimpleTestCase):
+
+    def setUp(self):
+        NCMock = NonCallableMock
+
+        self.subject = NCMock(spec=str)
+        self.body = NCMock(spec=str)
+        self.from_email = NCMock(spec=str)
+        self.to = NonCallableMagicMock(spec=list)
+        self.cc = NonCallableMagicMock(spec=list)
+        self.bcc = NonCallableMagicMock(spec=list)
+        self.reply_to = NCMock(spec=str)
+        self.fail_silently = NCMock(spec=bool)
+        self.auth_user = NCMock(spec=str)
+        self.auth_password = NCMock(spec=str)
+        self.connection = NCMock()
+        self.attachments = NCMock(spec=list)
+        self.headers = NonCallableMagicMock(spec=dict)
+
+    def test_simplest_call(self):
+        from lu_dj_utils.email import send_mail2
+
+        self.assertEqual(send_mail2(), 0)
+
+    def test_full_call(self):
+        """Test the function using all the parameters with mock objects."""
+        from lu_dj_utils.email import send_mail2
+
+        (subject, body, from_email, to, cc, bcc, reply_to, fail_silently,
+            auth_user, auth_password, connection, attachments, headers) = \
+        (self.subject, self.body, self.from_email, self.to, self.cc, self.bcc,
+            self.reply_to, self.fail_silently, self.auth_user,
+            self.auth_password, self.connection, self.attachments,
+            self.headers)
+        send_return_value = NonCallableMock()
+
+        with patch.object(django_mail.EmailMessage, '__init__', return_value=None) as mock_init:
+            with patch.object(django_mail.EmailMessage, 'send') as mock_send:
+                mock_send.return_value = send_return_value
+
+                rvalue = send_mail2(
+                    subject, body, from_email, to, cc,
+                    bcc, reply_to, fail_silently, auth_user, auth_password,
+                    connection, attachments, headers)
+
+                # check EmailMessage object construction
+                mock_init.assert_called_once_with(subject, body, from_email,
+                    to, bcc, connection, attachments, headers, cc)
+                # check the returned value is the result of EmailMessage's send method
+                self.assertEqual(rvalue, send_return_value)
+
+    def test_connection(self):
+        """Check which ``connection`` object is used to create the
+        :class:`EmailMessage` object.
+
+        If ``connection`` is passed, that one is used. If not, the result of
+        :func:`django.core.mail.get_connection` is used.
+
+        """
+        from lu_dj_utils.email import send_mail2
+
+        # Case A: if 'connection' is passed, that is used
+        with patch.object(django_mail.EmailMessage, '__init__', return_value=None) as mock_init:
+            with patch.object(django_mail.EmailMessage, 'send'):
+                send_mail2(connection=self.connection)
+
+                mock_init.assert_called_once_with(
+                    '', '', None, None, None,
+                    self.connection, None, None, None)
+
+        # Case B: else, the result of 'get_connection' is used
+        # (also, the arguments of that call are checked)
+        with patch('django.core.mail.get_connection') as mock_get_connection:
+            mock_connection = Mock(name='connection')
+            mock_get_connection.return_value = mock_connection
+
+            with patch.object(django_mail.EmailMessage, '__init__', return_value=None) as mock_init:
+                with patch.object(django_mail.EmailMessage, 'send'):
+                    send_mail2()
+
+                    mock_init.assert_called_once_with(
+                        '', '', None, None, None, mock_connection, None, None, None)
+                    mock_get_connection.assert_called_once_with(
+                        username=None, password=None, fail_silently=False)
+
+    def test_reply_to(self):
+        from lu_dj_utils.email import send_mail2
+
+        mock_conn = Mock(name='connection')
+
+        with patch('django.core.mail.get_connection') as mock_get_connection:
+            mock_get_connection.return_value = mock_conn
+
+            with patch.object(django_mail.EmailMessage, '__init__', return_value=None) as mock_init:
+                with patch.object(django_mail.EmailMessage, 'send'):
+
+                    send_mail2(headers=self.headers)
+                    mock_init.assert_called_once_with(
+                        '', '', None, None, None,
+                        mock_conn, None, self.headers, None)
+                    self.assertFalse(self.headers.__setitem__.called)
+
+                    mock_init.reset_mock()
+                    self.headers.reset_mock()
+
+                    send_mail2(reply_to=self.reply_to, headers=self.headers)
+                    mock_init.assert_called_once_with(
+                        '', '', None, None, None,
+                        mock_conn, None, self.headers, None)
+                    self.headers.__setitem__.assert_called_once_with(
+                        'Reply-To', self.reply_to)
+
+                    mock_init.reset_mock()
+                    self.headers.reset_mock()
+
+                    send_mail2(reply_to=self.reply_to)
+                    mock_init.assert_called_once_with('', '', None, None, None,
+                        mock_conn, None, {'Reply-To': self.reply_to}, None)
+                    self.assertFalse(self.headers.__setitem__.called)
+
+    def test_real_call(self):
+        from lu_dj_utils.email import send_mail2
+
+        _reset_test_outbox()
+        outbox = django_mail.outbox
+
+        subject = 'hello!'
+        body = 'my email body'
+        to = ['a@a.cl', 'x@xx.com']
+
+        self.assertEqual(len(outbox), 0)
+        send_mail2(subject=subject, body=body, to=to)
+        self.assertEqual(len(outbox), 1)
+        self.assertEqual(outbox[0].to, to)
+        self.assertIn(body, outbox[0].body)
+
+    def test_invalid_addresses(self):
+        from lu_dj_utils.email import send_mail2
+
+        _reset_test_outbox()
+        outbox = django_mail.outbox
+
+        subject = 'hello!'
+        body = 'my email body'
+        invalid_addresses = ['', ' ', 'x', 'x@x']
+        not_str_addresses = [None, -15]
+
+        # empty 'to' list: sends nothing
+        send_mail2(subject=subject, body=body, to=[])
+        self.assertEqual(len(outbox), 0)
+
+        # Even though the addresses are invalid, the messages to them ARE SENT
+        for address in invalid_addresses:
+            temp = len(outbox)
+            send_mail2(subject=subject, body=body, to=[address])
+            self.assertEqual(len(outbox), temp + 1)
+
+        # if address contains a newline, BadHeaderError(ValueError) is raised
+        with self.assertRaises(ValueError):
+            send_mail2(subject=subject, body=body, to=['\n'])
+
+        # if address is not a string, ValueError is raised
+        for address in not_str_addresses:
+            with self.assertRaises(ValueError):
+                send_mail2(subject=subject, body=body, to=[address])
+
+
+class SendTemplatedMailTest(TestCase):
+
+    def setUp(self):
+        self.valid_address = 'pedro@perez.cl'
+        self.invalid_address = 'pedro@perez'
+
+        self.username = NonCallableMock()
+        self.context = dict(nombre=self.username)
+
+        # these templates must exist
+        self.subject_name = 'common/tests/test_welcome_email_subject.txt'
+        self.body_name = 'common/tests/test_welcome_email_body.txt'
+        self.html_body_name = 'common/tests/test_html_body.html'
+
+    def test_enviar_correo_with_valid_address(self):
+        address = self.valid_address
+        self.assertTrue(self._send_default(address))
+
+    def test_enviar_correo_with_invalid_address(self):
+        """It **will** send the message, even if the address is invalid."""
+        address = self.invalid_address
+        self.assertTrue(self._send_default(address))
+
+    def test_enviar_correo(self):
+        _reset_test_outbox()
+        outbox = django_mail.outbox
+
+        address = self.valid_address
+
+        # there is no emails before sending the email
+        self.assertEqual(len(outbox), 0)
+
+        # we trigger the action that send the email
+        self._send_default(address)
+
+        # email actually sent?
+        self.assertEqual(len(outbox), 1)
+
+        # the email is sent to the right destination
+        self.assertEqual(outbox[0].to, [address])
+        self.assertEqual(outbox[0].from_email, settings.DEFAULT_FROM_EMAIL)
+
+    def test_html(self):
+        from lu_dj_utils.email import render_to_string, send_templated_mail
+
+        _reset_test_outbox()
+        outbox = django_mail.outbox
+        self.assertEqual(len(outbox), 0)
+
+        email_address = self.valid_address
+
+        result = send_templated_mail(
+            self.context, self.subject_name, self.body_name,
+            self.html_body_name, to=[email_address])
+        self.assertTrue(result)
+        self.assertEqual(len(outbox), 1)
+
+        message = outbox[0]
+        html_body = render_to_string(self.html_body_name, self.context)
+        html_alternative = message.alternatives[0]
+        self.assertEqual(html_alternative[0], html_body)
+        self.assertEqual(html_alternative[1], 'text/html')
+
+    def _send_default(self, email_address):
+        from lu_dj_utils.email import send_templated_mail
+
+        return send_templated_mail(
+            self.context, self.subject_name, self.body_name, to=[email_address])
+
+
+class Functions(SimpleTestCase):
+
+    # noinspection PyTypeChecker
+    def test_set_header_reply_to(self):
+        from lu_dj_utils.email import set_header_reply_to
+
+        func = set_header_reply_to
+
+        email1 = NonCallableMock(spec=str)
+        email2 = NonCallableMock(spec=str)
+
+        self.assertEqual(func(None, email1), {'Reply-To': email1})
+        self.assertEqual(func({}, email1), {'Reply-To': email1})
+        self.assertEqual(func({'Reply-To': email1}, email2), {'Reply-To': email2})
+        self.assertIsNone(func(None, None))
+        self.assertEqual(func({}, None), {})
+
+
+def _local_file_abspath(filename):
+    import os
+    return os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), filename)
+
+
+def _template_file_abspath(filename):
+    import os
+    return os.path.join(
+        os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+        'templates', 'common', 'tests', filename)
+
+
+def _reset_test_outbox():
+    """Empty the outbox manually.
+
+    Apparently there is a bug in TestCase because the test runner should clear
+    the contents of the test email outbox at the start of each test case,
+    and it is not doing that.
+
+    https://docs.djangoproject.com/en/dev/topics/testing/overview/#emptying-the-test-outbox
+
+    """
+    django_mail.outbox = []

54370c7e266e55c8b9330cc8621f33861c58d196
Germán Larraín <glarrain@users.noreply.github.com>
2015-11-12 11:03:19 -0300

    runtests: customize `DEFAULT_SETTINGS`
    - `INSTALLED_APPS`: add `django.contrib.sites`
    - `TEMPLATE_DIRS`: add a dir with templates for tests
    - `LOGGING`: we want to see all warnings and erros in console

--- runtests.py
+++ runtests.py
@@ -11,15 +11,60 @@ from django.conf import settings
 import django
 
 
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+LOGGING_CONFIG = {
+    'version': 1,
+    # 'disable_existing_loggers': True,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'custom': {
+            # available LogRecord attributes:
+            # http://docs.python.org/library/logging.html#logrecord-attributes
+            'format': ('%(levelname)s [%(asctime)s] %(name)s | '
+                       '%(funcName)s:%(lineno)d | %(message)s\n')
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'custom'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', ],
+            'level': 'WARNING',
+            'propagate': False,
+        },
+        'django.request': {
+            'handlers': ['console', ],
+            'level': 'WARNING',
+            'propagate': False,
+        },
+        'lu_dj_utils': {
+            'handlers': ['console', ],
+            'level': 'WARNING',
+            'propagate': False,
+        },
+    }
+}
+
 DEFAULT_SETTINGS = dict(
     INSTALLED_APPS=(
+        'django.contrib.sites',  # used by `lu_dj_utils.urls`
         'lu_dj_utils',
-        ),
+    ),
     DATABASES={
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             }
         },
+    TEMPLATE_DIRS=(
+        os.path.join(ROOT_DIR, 'tests', 'templates'),
+    ),
+    LOGGING=LOGGING_CONFIG,
     SILENCED_SYSTEM_CHECKS=[
         '1_7.W001',  # MIDDLEWARE_CLASSES is not set
     ],
